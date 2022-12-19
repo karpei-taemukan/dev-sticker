1f7e6864ce7db07dc86d3328ffa4b7bcfeb25017
karpeitaemukan <l07wkdgustn07l@gmail.com>
2022-12-20 00:09:23 +0900

    how to query gatsby

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react"
 
 interface ISeoProps{
@@ -5,5 +6,19 @@ interface ISeoProps{
 }
 
 export default function Seo({title}:ISeoProps){
-    return <title>{title} | DevStickers</title>
+    const data = useStaticQuery<Queries.SeoDataQuery>(graphql`
+    query SeoData{
+        site{
+          siteMetadata{
+            title
+          }
+        }
+      }
+    `);
+      // Gatsby가  웹사이트의 html을 만들고 있을때
+      //(빌드(npm run build)할때//유저가 웹사이트에 도착하기 전)
+      // useStaticQuery를 이용해 data를 받아온다
+    console.log(data)
+    console.log(data.site?.siteMetadata?.title)
+    return <title>{title} | {data.site?.siteMetadata?.title}</title>
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { graphql, PageProps } from "gatsby";
 
-export default function Blog(){
+export default function Blog({data} : PageProps<Queries.BlogTitlesQuery>){
+    console.log(data);
     return (
        <Layout title="Blog">
-        <p>We are happies sticker store.</p>
+        <ul>
+          {data.allFile.nodes.map((file,index) => <li key={index}>{file.name}</li>)}
+        </ul>
        </Layout>
     )
 }
 
 export const Head = () => <Seo title="Blog" />;
   
+// Gatsby는 html 먼저 로드한 후에 react를 로드한다
+
+export const query = graphql` 
+query BlogTitles{
+    allFile{
+      nodes{
+        name
+      }
+    }
+    }
+`;
+
+// query는 data를 가져오고 싶을때 쓴다
